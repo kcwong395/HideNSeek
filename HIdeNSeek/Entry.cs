f4374e32af8fcfe296a7d5a8c84a69dae25f115a
ElderHorse <kcwong395@gmail.com>
2019-08-18 17:24:13 +0800

    Initial the form

---
+++ HIdeNSeek/Entry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HideNSeek
+{
+    class Entry
+    {
+        static void Main(string[] imgs)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // If args length is not 0, a file is opened with this application
+            if (imgs.Length == 0)
+            {
+                Application.Run(new HideNSeek(imgs));
+            }
+            else
+            {
+                Application.Run(new HideNSeek());
+            }
+        }
+    }
+}
---
+++ HIdeNSeek/HideNSeek.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HideNSeek
+{
+    public partial class HideNSeek : Form
+    {
+        public HideNSeek()
+        {
+            InitializeComponent();
+        }
+
+        public HideNSeek(string[] imgs)
+        {
+            InitializeComponent();
+        }
+
+        private void HideNSeek_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [STAThread]
+        private void Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread t = new Thread(() => {
+                    OpenFileDialog dlg = new OpenFileDialog
+                    {
+                        Title = "Select an image",
+                        InitialDirectory = "C:/"
+                    };
+
+                    // The following would not return the dialog if the current
+                    // thread is not STA
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        status.Text = "File Selected: " + dlg.FileName;
+                    }
+
+                });
+                t.SetApartmentState(ApartmentState.STA);
+                t.Start();
+            }
+            catch (Exception) { }
+        }
+
+        private void Hide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Seek_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
