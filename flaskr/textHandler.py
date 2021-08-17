aac504dfa95ade441721916c2ab8832ad2cf6c8d
kcwong395 <kcwong395@gmail.com>
2021-08-17 09:46:12 -0400

    complete functions to encode and decode message to utf-8 format

---
+++ flaskr/textHandler.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class TextHandler:
+    def __init__(self):
+        pass
+
+    def encodeMsg(self, msg: str) -> List[List[int]]:
+        encoded_msg = msg.encode('utf-8')
+        return [[(byte >> i) & 1 for i in range(7, -1, -1)] for byte in encoded_msg]
+
+    def decodeMsg(self, byte_arr: List[List[int]]) -> str:
+        byte_str = bytearray()
+        for byte in byte_arr:
+            tmp = 0
+            for bit in byte:
+                tmp = tmp * 2 + bit
+            byte_str.append(tmp)
+        return byte_str.decode('utf-8')
