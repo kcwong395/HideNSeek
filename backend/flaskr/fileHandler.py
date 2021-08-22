b07359044313447f9872936e4c5e54e765d76893
kcwong395 <kcwong395@gmail.com>
2021-08-22 11:57:21 -0400

    unused code

--- backend/flaskr/fileHandler.py
+++
@@ -1,6 +0,0 @@
-class FileHandler:
-    def __init__(self):
-        self.ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-    def allowed_file(self, filename):
-        return '.' in filename and filename.rsplit('.', 1)[1] in self.ALLOWED_EXTENSIONS
