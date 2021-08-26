3a02a9e14778f611343502f059a7652f0e5d7edf
kcwong395 <kcwong395@gmail.com>
2021-08-26 10:03:07 -0400

    make var not protected

--- backend/flaskr/textHandler.py
+++ backend/flaskr/textHandler.py
@@ -3,19 +3,19 @@ from typing import List, Optional, Final
 
 class TextHandler:
 
-    _HEADER: Final = '<STR>'
-    _FOOTER: Final = '<END>'
+    HEADER: Final = '<STR>'
+    FOOTER: Final = '<END>'
 
     @staticmethod
     def remove_indicator(msg: str) -> Optional[str]:
-        if not msg.startswith(TextHandler._HEADER) or not msg.endswith(TextHandler._FOOTER):
+        if not msg.startswith(TextHandler.HEADER) or not msg.endswith(TextHandler.FOOTER):
             return None
         else:
-            return msg[len(TextHandler._HEADER):len(TextHandler._FOOTER) * -1]
+            return msg[len(TextHandler.HEADER):len(TextHandler.FOOTER) * -1]
 
     @staticmethod
     def insert_indicator(msg: str) -> str:
-        return TextHandler._HEADER + msg + TextHandler._FOOTER
+        return TextHandler.HEADER + msg + TextHandler.FOOTER
 
     @staticmethod
     def encode_msg(msg: str) -> List[List[int]]:
