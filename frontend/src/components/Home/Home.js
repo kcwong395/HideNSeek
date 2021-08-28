d2b9b40632dca03f494263c72ae862e6f52ee24d
kcwong395 <kcwong395@gmail.com>
2021-08-28 11:08:34 -0400

    add demo viedo and map backend port

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from '@material-ui/core'
 import Banner from './Banner/Banner';
-import { makeStyles } from '@material-ui/core/styles';
 
 export default function Home() {
 
@@ -9,6 +8,12 @@ export default function Home() {
         <React.Fragment>
             <Container maxWidth="lg" style={{ textAlign: 'center' }}>
                 <Banner />
+            </Container>
+            <Container maxWidth="lg" style={{ textAlign: 'center' }}>
+            <video width="100%" height="100%" controls>
+                <source src="HideNSeek-Demo.mkv" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video> 
             </Container>
         </React.Fragment>
     )
